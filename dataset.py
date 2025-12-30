aa4ab197ae28724a0fca3730c4dbca9276c8778a
David Fabian <usernamedf@Outlook.com>
2025-12-30 08:27:13 -0500

    added random jpeg compression

--- dataset.py
+++ dataset.py
@@ -1,53 +1,20 @@
+import torch
+from torchvision.transforms import v2
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from PIL import Image
 from sklearn.model_selection import train_test_split
-from torchvision import transforms
 Image.MAX_IMAGE_PIXELS = None
-
-class ImgResize(object):
-    def __call__(self, img: Image, output_size=(224,224)):
-        image = img
-
-        original_width, original_height = image.size
-        aspect_ratio = original_width / original_height
-
-        target_width, target_height = output_size
-        if target_width / target_height > aspect_ratio:
-            target_width = int(target_height * aspect_ratio)
-        else:
-            target_height = int(target_width / aspect_ratio)
-
-        # Resize the image
-        resized_image = image.resize((target_width, target_height), Image.LANCZOS)
-
-        return resized_image
-
-class PadToSquare:
-    def __call__(self, img: Image):
-
-        width, height = img.size
- 
-        target_size = max(width, height)
-
-        new_img = Image.new("RGB", (target_size, target_size), (0, 0, 0))
-  
-        x_offset = (target_size - width) // 2
-        y_offset = (target_size - height) // 2
-        
-        new_img.paste(img, (x_offset, y_offset))
-        
-        return new_img
 
 BATCH_SIZE = 512
 
-transform = transforms.Compose([
-    ImgResize(),
-    PadToSquare(),
-    transforms.ToTensor(),
-    transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+transform = v2.Compose([
+    v2.ToImage(),
+    v2.Resize(256, antialias=True),
+    v2.CenterCrop(224),
+    v2.RandomApply([v2.JPEG((30, 90))], p=0.5),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 dataset=ImageFolder("archive", transform=transform)
