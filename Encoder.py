648a9c5ae5f42fe4e43316671a00037163ac77cb
David Fabian <usernamedf@Outlook.com>
2026-03-10 12:06:49 -0400

    encoder with moe

--- Encoder.py
+++ Encoder.py
@@ -1,8 +1,8 @@
-import torch
 from torch import nn
 from Attention import MultiHeadAttention
+from MixtureOfExperts import MOE
 
-class Encoder(nn.Module):
+class EncoderMLP(nn.Module):
     def __init__(self, embed_dim, num_heads):
         super().__init__()
         
@@ -22,4 +22,19 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = x + self.MSA(self.LN1(x))
         x = x + self.dropout(self.MLP(self.LN2(x)))
+        return x
+    
+class EncoderMOE(nn.Module):
+    def __init__(self, num_experts, expert_size, k, bias_param, embed_dim, num_heads, bn, pn):
+        super().__init__()
+        
+        self.MSA = MultiHeadAttention(embed_dim, num_heads)
+        self.MOE = MOE(num_experts, expert_size, k, bias_param, embed_dim, bn, pn)
+
+        self.LN1 = nn.LayerNorm(embed_dim)
+        self.LN2 = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = x + self.MSA(self.LN1(x))
+        x = x + (self.MOE(self.LN2(x)))
         return x
